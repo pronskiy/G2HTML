bf6f43b1cef51f7b118524abbb7d113cc8d53a27
stanislav.dombrovsky <stanislav.dombrovsky@jetbrains.com>
2021-04-01 15:10:57 +0300

    v.59: add ability to clear the list of errors together with bookmarks

--- settings/constants.js
+++ settings/constants.js
@@ -1,4 +1,4 @@
 var SETTINGS_STORAGE_NAME = "g2html_settings.json";
-var VERSION = 58;
+var VERSION = 59;
 var SHORTCUTS = "[⌘⌥⇧⇥⌃↓←→↑⏎]|\\bF([1-9]|1[0-2])\\b";
 var URL_TOKENS = "(&|\\?)(_ga|_gac|_gl|fbclid|mkt_tok|gtm_[^&]*|utm_[^&]*)(=[^&|#]*)"
