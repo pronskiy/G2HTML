cee25981e9b9c157f2a24a1de0cc96aa2bcb6382
stanislav.dombrovsky <stanislav.dombrovsky@jetbrains.com>
2021-04-26 08:21:32 +0300

    Remove not needed functions

--- script.js
+++ script.js
@@ -1,23 +1,5 @@
 //Global storage for messages
 var messages = [];
-var progress = 0;
-var total = 0;
-
-function calculateTotal(item) {
-    if (!item.getNumChildren) {
-        return 0;
-    }
-    var current = 0;
-    var numChildren = item.getNumChildren();
-    for (var i = 0; i < numChildren; i++) {
-        current++;
-        var child = item.getChild(i);
-        if (child !== null && child.getNumChildren) {
-            current += calculateTotal(child);
-        }
-    }
-    return current;
-}
 
 function onInstall(e) {
     onOpen(e);
