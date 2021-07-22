b87302a910813be0e7bbc37e89da1e883ec62322
stanislav.dombrovsky <stanislav.dombrovsky@jetbrains.com>
2021-07-22 19:03:05 +0300

    v.84 - enable v8 runtime again.

--- settings/constants.js
+++ settings/constants.js
@@ -1,5 +1,5 @@
 var SETTINGS_STORAGE_NAME = "g2html_settings.json";
-var VERSION = 81;
+var VERSION = 84;
 var SHORTCUTS = "[⌘⌥⇧⇥⌃↓←→↑⏎]|\\bF([1-9]|1[0-2])\\b";
 var URL_TOKENS = "(&|\\?)(_ga|_gac|_gl|fbclid|mkt_tok|gtm_[^&]*|utm_[^&]*)(=[^&|#]*)"
 var PLUGIN_NAME = "Export to HTML"
