7f0832e7f0841d6a477093d9aa8bf8441e8edfb3
stanislav.dombrovsky <stanislav.dombrovsky@jetbrains.com>
2021-12-01 13:04:32 +0300

    Ok, warmup fails, let's try the other way.

--- warmup.sh
+++ warmup.sh
@@ -1,9 +1,9 @@
-apt update
-apt install -y zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-apt install -y npm
-npm install -g n
-n stable
-PATH="$PATH"
-export PATH="$PATH"
-npm install -g @google/clasp
+#apt update
+#apt install -y zsh
+#sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+#apt install -y npm
+#npm install -g n
+#n stable
+#PATH="$PATH"
+#export PATH="$PATH"
+#npm install -g @google/clasp
