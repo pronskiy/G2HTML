00dcaffa40ceac58f589737c8dec7e5235ee3a6d
stanislav.dombrovsky <stanislav.dombrovsky@jetbrains.com>
2021-12-01 12:36:12 +0300

    Add warmup scripts / job setup

---
+++ .space.kts
@@ -0,0 +1,7 @@
+job("G2HML warmup") {
+
+    warmup(ide = Ide.Fleet) {
+        // path to the warm-up script
+        scriptLocation = "./warmup.sh"
+    }
+}
---
+++ warmup.sh
@@ -0,0 +1,7 @@
+apt update
+apt install zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+apt install npm
+npm install -g n
+n stable
+npm install -g @google/clasp
